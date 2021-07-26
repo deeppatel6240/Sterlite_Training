353ecbde0c1a5a2523888be9caa4c1915ec76877
Sherathiya Deep <sherathiyadeep2469@gmail.com>
2021-07-26 10:49:51 +0530

    Add files via upload

---
+++ Maven_Eclipse-workspace/Maven-Project_using_Eclipse/online-shop-webapp/src/main/java/com/phoenix/servlets/HelloServlet.java
@@ -0,0 +1,36 @@
+package com.phoenix.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<h1>Welcome to Maven Using Servlet</h1>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ Maven_Eclipse-workspace/Maven-Project_using_Eclipse/online-shop-webapp/target/online-shop-webapp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+<p><a href="hello">Click here</a></p>
+</body>
+</html>
