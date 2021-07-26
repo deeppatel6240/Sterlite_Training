bc2ec37e5cb75009a73968da87984a36caec379c
Sherathiya Deep <sherathiyadeep2469@gmail.com>
2021-07-26 10:35:28 +0530

    Add files via upload

---
+++ Maven_Eclipse-workspace/my-java-app/src/main/java/com/phoenix/hello/App.java
@@ -0,0 +1,14 @@
+package com.phoenix.hello;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+    }
+}
