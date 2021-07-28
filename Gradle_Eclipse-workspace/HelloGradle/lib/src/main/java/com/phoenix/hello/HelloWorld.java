f09581f5aaf0196641d5fd979940585c033fa912
Sherathiya Deep <sherathiyadeep2469@gmail.com>
2021-07-28 10:39:21 +0530

    Add files via upload

---
+++ Gradle_Eclipse-workspace/HelloGradle/lib/src/main/java/com/phoenix/hello/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.phoenix.hello;
+/*
+ * Creator Name: deepkumar.sherathiya@stltech.in
+ * Date: 27/07/2021
+ * Version: 1.0
+ * Copyright: Sterlite Technologies Ltd.
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello From Gradle");
+		//System.out.println(10/0);
+
+	}
+
+}
